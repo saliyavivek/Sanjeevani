31cdb2917bcd3028c2a81717ff802a98fd48eb39
Vivek Saliya <viveksaliya004@gmail.com>
2025-02-24 16:52:09 +0530

    warning

--- frontend/src/pages/Maintenance.jsx
+++ frontend/src/pages/Maintenance.jsx
@@ -1,6 +1,6 @@
 export default function Maintenance() {
   return (
-    <div className="h-screen flex flex-col justify-center mx-4">
+    <div className="h-screen flex flex-col justify-center mx-4 md:items-center">
       <p className="text-gray-500 text-md mb-6">
         Oh look, who is here! <br />
       </p>
@@ -8,10 +8,11 @@ export default function Maintenance() {
         Finally, you remembered this website huh? I guess 400 marks were not
         enough motivation for you.
       </p>
-      <p className=" text-gray-500 text-md mb-6">
+      <p className="text-gray-500 text-md mb-6">
         If you’ve finally decided to open this site, message me so I at least
         know you care about <b>"our"</b> project. Otherwise, enjoy this page.
       </p>
+      <br />
       <p className="text-gray-500 text-md mb-6">
         પઘારો! <br />
       </p>
@@ -19,10 +20,11 @@ export default function Maintenance() {
         અરે, તમને આ વેબસાઇટ યાદ છે હજૂ? મને લાગ્યૂ કે 400 માર્કસ તમારા માટે
         પૂરતૂ મોટિવેશન નથી.
       </p>
-      <p className=" text-gray-500 text-md mb-6">
+      <p className="text-gray-500 text-md mb-6">
         જો તમે આખરે આ સાઇટ ખોલવાનું નક્કી કર્યું જ હોય, તો મને જણાવો જેથી મને
         ખબર પડે કે તમને <b>"આપણા"</b> પ્રોજેક્ટ ની કંઈક પડી છે.
       </p>
+      <p className="text-gray-500 text-md mb-6">24.02.2025</p>
     </div>
   );
 }
